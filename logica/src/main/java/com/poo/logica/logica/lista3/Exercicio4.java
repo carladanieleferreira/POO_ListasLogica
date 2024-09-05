36338dc58cb932695fe626464b9af8cf3c17995d
carladanieleferreira <daniele.fersan@gmail.com>
2024-09-05 13:07:39 -0300

    Resolução Exercicio 7 - Lista 3 - Tendo por base o explicado em aula

--- logica/src/main/java/com/poo/logica/logica/lista3/Exercicio4.java
+++ logica/src/main/java/com/poo/logica/logica/lista3/Exercicio4.java
@@ -35,7 +35,7 @@ public static void resolucao(){
             System.out.println("Não desista ... tá morno, quase lá...  Tente novamente: ");
         }else if(tentativa > (certo + 10 ) || tentativa < (certo - 10 )){
             System.out.println("Não foi dessa vez ainda ... porém tá quente ...  Tente novamente: ");
-        }else if (tentativa >= (certo + 1 ) || tentativa <= (certo - 1 )){
+        }else if (tentativa > (certo + 1 ) || tentativa <= (certo - 1 )){
             System.out.println("Te contar um segredo: você está fervendo... Tente novamente: ");
         }else if (tentativa == certo ){
             acertou = true ;
--- logica/src/main/java/com/poo/logica/logica/lista3/Exercicio7.java
+++ logica/src/main/java/com/poo/logica/logica/lista3/Exercicio7.java
@@ -8,23 +8,23 @@ public class Exercicio7 {
     
 /* Escreva um programa que seja capaz de desejar um retangulo */
 
-int largura;
-int altura;
-Scanner leia = new Scanner(System.in);
+        int largura;
+        int altura;
+        
+        System.out.println("Digite a Altura desejada para o retângulo: ");
+        Scanner leia = new Scanner(System.in);
+        altura = leia.nextInt();
 
-System.out.println("Altura ");
-altura = leia.nextInt();
-
-System.out.println("Largura ");
-largura = leia.nextInt();
-
-for (int j = 0; j < altura; j++) {
-    for (int i = 0; i < largura; i++) {
-    System.out.println("*");
-}
-    System.out.println();
-}
-leia.close();
-}
-
-}
+        System.out.println("Digite a Largura desejada para o retângulo: ");
+        largura = leia.nextInt();
+        
+        for (int j = 0; j < altura; j++) {
+            for (int i = 0; i < largura; i++) {
+                System.out.print("*");
+            }  
+            System.out.println();
+        }
+        leia.close();
+    }
+    
+    }
