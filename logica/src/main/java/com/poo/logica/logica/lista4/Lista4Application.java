8917dd8248ab158ccf890e626915cd91b6e09df1
carladanieleferreira <daniele.fersan@gmail.com>
2024-09-07 21:07:58 -0300

    Resolução Lista 4 - Exercicio 8

--- logica/src/main/java/com/poo/logica/logica/lista4/Lista4Application.java
+++ logica/src/main/java/com/poo/logica/logica/lista4/Lista4Application.java
@@ -9,6 +9,6 @@ public class Lista4Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Lista4Application.class, args);
 
-		Exercicio7.resolucao();
+		Exercicio8.resolucao();
 	}
 }
